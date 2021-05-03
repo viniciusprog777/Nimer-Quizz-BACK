977a584a1a29156d8f85bfdf7861eaaad1b00f72
viniciusprog777 <viniciusjetmax@gmail.com>
2021-05-03 11:44:56 -0300

    Ajustes

--- src/database/index.js
+++ src/database/index.js
@@ -2,13 +2,19 @@ const Sequelize = require("sequelize");
 
 const dbConfig = require("../config/database");
 
-const Teacher = require("../models/Teacher");
-const Student = require("../models/Student");
-const Institution = require("../models/Institution");
-const Course = require("../models/Course");
+const Achievement = require("../models/Achievement");
+const Answer = require("../models/Answer");
 const Class = require("../models/Class");
-const Level = require("../models/Level");
+const Contract = require("../models/Contract");
+const Course = require("../models/Course");
+const Question = require("../models/Question");
+const Quizz = require("../models/Quizz");
+const Student = require("../models/Student");
+const Teacher = require("../models/Teacher");
+const Theme = require("../models/Theme");
 const User = require("../models/User");
+const Institution = require("../models/Institution");
+const Level = require("../models/Level");
 
 const conex = new Sequelize(dbConfig.url, dbConfig.config);
 
@@ -19,6 +25,12 @@ Course.init(conex);
 Class.init(conex);
 Level.init(conex);
 User.init(conex);
+Achievement.init(conex);
+Answer.init(conex);
+Theme.init(conex);
+Contract.init(conex);
+Question.init(conex);
+Quizz.init(conex);
 
 Student.associate(conex.models);
 Teacher.associate(conex.models);
@@ -27,6 +39,12 @@ Course.associate(conex.models);
 Class.associate(conex.models);
 Level.associate(conex.models);
 User.associate(conex.models);
+Achievement.associate(conex.models);
+Answer.associate(conex.models);
+Theme.associate(conex.models);
+Contract.associate(conex.models);
+Question.associate(conex.models);
+Quizz.associate(conex.models);
 
 for (let assoc of Object.keys(Teacher.associations)) {
   for (let accessor of Object.keys(Teacher.associations[assoc].accessors)) {
---
+++ src/models/Quizz.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require("sequelize");
+
+class Quizz extends Model {
+    static init(sequelize){
+        super.init(
+            {
+        
+            },
+            {
+                tableName: "quizz",
+                sequelize,
+            }
+        );
+    }
+    static associate(models){
+        this.belongsTo(models.Teacher, { foreignKey: "teacher_id"});
+        this.belongsTo(models.Class, { foreignKey: "class_id"});
+        this.belongsToMany(models.Quizz, { foreignKey: "quizz_id"});
+    }
+}
+module.exports = Quizz;
